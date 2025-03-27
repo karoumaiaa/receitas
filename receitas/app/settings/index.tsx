b6489bc7bc5886cdf62a004df7cf358c3b5fee42
Karoumaia <karolainepinzmaia@gmail.com>
2025-03-26 21:40:55 -0300

    primeiro commit

---
+++ receitas/app/settings/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Inicio() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Um pouco sobre a construção da página</Text>
+    </View>
+  );
+}
